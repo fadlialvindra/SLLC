e0d736fe2a93cb1c8aedc8f29a24fcbd96bffd95
Muhammad Reza Anggara <38530689+muhammadreza1044@users.noreply.github.com>
2018-05-01 00:11:29 +0700

    Update SLLC.cpp

--- SLLC.cpp
+++ SLLC.cpp
@@ -9,6 +9,7 @@ class Node
 };
 
 Node *head;
+Node *del;
 
 void init()
 {
@@ -20,12 +21,82 @@ bool isEmpty()
         return 1;
     return 0;
 }
+void barudepan(int databaru){
+	Node *baru,*bantu;
+	baru = new Node;
+	baru->data = databaru;
+	baru->next = baru;
+	if(isEmpty()==1){
+	 head=baru;
+	 head->next=head;
+	}
+	else {
+		bantu = head;
+		while(bantu->next!=head){
+			bantu=bantu->next;
+		}
+		baru->next = head;
+		head = baru;
+      	bantu->next = head;
+	}
+	cout<<"Data masuk\n";
+}
+
+void hapuspalak(){
+	int bantu;
+	if (head==NULL){
+		cout<<"\nData kosong"<<endl;
+	} else {
+		bantu = head->data;
+		del = head;
+		head = head->next;
+		delete del;
+		
+		cout<<"\nData yang diapus adalah "<<bantu<<endl;
+	}
+}
+
+void menu(){
+	int pilih, data;
+	char ulang;
+	do{
+		system("cls");
+      cout<<"Tugas SLLC MANTAP"<<endl;
+      cout<<"================="<<endl;
+      cout<<"Menu : "<<endl;
+      cout<<"1. Input data"<<endl;
+      cout<<"2. Hapus data"<<endl;
+      cout<<"3. Exit"<<endl;
+      cout<<"Your Choice : ";
+      cin>>pilih;
+
+      switch(pilih)
+      {
+      case 1 :
+            cout<<"\nInput Data : ";
+            cin>>data;
+            barudepan(data);
+            break;
+      case 2 :
+            hapuspalak();
+            break;
+      case 3 :
+            exit(0);
+            break;
+      default :
+            cout<<"\nPilih ulang"<<endl;
+      }
+      cout<<"\nBack to Menu?(y/n)";
+      cin>>ulang;
+      }while(ulang=='y' || ulang=='Y');
+		
+	}
 
 
 int main()
 {
     init();
-    
+    menu();
     
     return 0;
 }
